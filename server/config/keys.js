b7710cf4955b65feefb50af189c3098402ec99b2
Liork1 <liorkooznits1@gmail.com>
2019-09-26 17:43:09 +0300

    Server init #1

---
+++ server/config/keys.js
@@ -0,0 +1,6 @@
+module.exports = {
+    googleClientID: '235128139431-l1uc6t3pdtrbs2j82uvdi9nnbcp4dmdd.apps.googleusercontent.com',
+    googleClientSecret: 'nzdBV94_5rxt2cF_Z1B4l8sn',
+    mongoURI: 'mongodb+srv://dbLior:ZB9IZhymkircHwvd@emaily-togbw.mongodb.net/test?retryWrites=true&w=majority',
+    cookieKey: 'dfdsdffdskjkjerdffsdfsdfsdfdsfdsfd'
+}
